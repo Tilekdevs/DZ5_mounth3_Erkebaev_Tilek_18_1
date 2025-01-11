e62956a13ca661927e07cbce89869231d8b6b906
Tilek Erkebaev <108805434+Tilekdevs@users.noreply.github.com>
2025-01-12 00:07:31 +0600

    Update app.js
    update old code

--- hw_5/app.js
+++ hw_5/app.js
@@ -2,37 +2,30 @@ const som = document.querySelector("#som");
 const usd = document.querySelector("#usd");
 const euro = document.querySelector("#euro");
 
-
 const convert = (elem, target, target2) => { 
   elem.addEventListener("input", () => { 
-    const reqest = new XMLHttpRequest(); 
-    reqest.open("GET", "main.json"); 
-    reqest.setRequestHeader("Content-type", "application/json"); 
-    reqest.send(); 
-    reqest.addEventListener("load", () => { 
-      const response = JSON.parse(reqest.response); 
-      if (elem === som) { 
-        target.value = (elem.value / response.usd).toFixed(2); 
-			target2.value = (elem.value / response.euro).toFixed(2);
-      } else if (elem === usd) { 
-        target.value = (elem.value * response.usd).toFixed(2);
-			target2.value = (elem.value * response.usd / response.euro).toFixed(2);
-      } else if (elem === euro) { 
-        target.value = (elem.value * response.euro).toFixed(2); 
-        target2.value = (elem.value * response.euro / response.usd ).toFixed(2); 
-      } 
-      elem.value === "" ? (target.value = "") : null, 
-      elem.value === "" ? (target2.value = "") : null;
-    }); 
+    fetch("main.json")
+      .then(response => response.json())
+      .then(data => {
+        if (elem === som) { 
+          target.value = (elem.value / data.usd) / 2; 
+          target2.value = (elem.value / data.euro) / 2;
+        } else if (elem === usd) { 
+          target.value = (elem.value * data.usd) / 2;
+          target2.value = (elem.value * data.usd / data.euro) / 2;
+        } else if (elem === euro) { 
+          target.value = (elem.value * data.euro) / 2; 
+          target2.value = (elem.value * data.euro / data.usd ) / 2; 
+        } 
+        if (elem.value === "") {
+          target.value = "";
+          target2.value = "";
+        }
+      })
+      .catch(error => console.error('Error:', error));
   }); 
 };
 
 convert(som, usd, euro);
 convert(euro, som, usd);
 convert(usd, som, euro);
-
-
-
-
-
-
